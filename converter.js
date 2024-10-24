d96fdcf78307bdef5ee2ae7e9daa6f55900d046f
Bludidi <bludidi5@gmail.com>
2024-10-24 22:05:45 +0200

    Work on converter function

--- converter.js
+++ converter.js
@@ -1,6 +1,6 @@
 
 const base_url_1 = "https://restcountries.com/v3.1/name/";
-const base_url_2 = "GET https://v6.exchangerate-api.com/v6/"
+const base_url_2 = "https://v6.exchangerate-api.com/v6/"
 const api_key = "45cb484fe555a71408c5de53"
 
 
@@ -12,21 +12,33 @@ function convertCurrency(country1, country2, amount) {
   const baseCountryUrl = base_url_1 + baseCountry;
   const targetCountryUrl = base_url_1 + targetCountry;
   const currUrl = base_url_2 + api_key + "/pair/";
-  //console.log(targetCountryUrl);
 const getData = async () => {
-  
   try {
     country1 = await axios.get(baseCountryUrl);
     country2 = await axios.get(targetCountryUrl);
-   // const convertWithAmount = await axios.get(currUrl + base.currencies + "/" + target.currencies + "/" + amount); 
     const base = country1.data[0];
     const target = country2.data[0];
     const baseCurrencyName = Object.keys(base.currencies)[0];
     const targetCurrencyName = Object.keys(target.currencies)[0];
     const baseCurrencyData = base.currencies[baseCurrencyName];
     const targetCurrencyData = target.currencies[targetCurrencyName];
+    const convertWithAmount = await axios.get(currUrl + baseCurrencyName + "/" + targetCurrencyName + "/" + amount);
 
-    console.log(baseCurrencyData.symbol, targetCurrencyData.symbol);
+    if (amount == "") {
+     alert ("Please enter amount to be converted");
+    } if (baseCountry === targetCountry) {
+      alert("Please enter a different target country");
+    } else {
+      const baseAmount = baseCurrencyData.symbol + amount;
+      const newAmount = targetCurrencyData.symbol + convertWithAmount.data.conversion_result;
+    document.getElementById("new-amount").innerHTML = baseAmount + " = " + newAmount;
+    const baseImg = document.getElementById("base-flag");
+    const targetImg = document.getElementById("target-flag");
+    baseImg.setAttribute("src", base.flags.svg);
+    targetImg.setAttribute("src", target.flags.svg);
+    }
+
+    
   } catch (error) {
     return error.message;
   }
