ee95c85cf9ef5924c5cfd8add3b0f679d5f25d5d
Bludidi <bludidi5@gmail.com>
2024-11-19 10:09:56 +0200

    Add modal and mobile nav

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 // JavaScript
 
-  let total = 0;
-  document.getElementById('count-text').textContent = total;
+//   let total = 0;
+//   document.getElementById('count-text').textContent = total;
 
-  function add() {
-    total += 1;
-    document.getElementById('count-text').textContent = total;
+//   function add() {
+//     total += 1;
+//     document.getElementById('count-text').textContent = total;
+//   }
+
+
+//   function minus() {
+//     total -= 1;
+//     document.getElementById('count-text').textContent = total;
+//   }
+
+// function resetCounter() {
+//   total = 0;
+//   document.getElementById('count-text').textContent = total;
+// }
+
+/*Nav and Modal */
+
+const openMenu = document.querySelector(".open-modal");
+const closeMenu = document.querySelector(".close-modal");
+const modal = document.querySelector(".modal");
+
+function openModal() {
+    modal.style.display = "block";
+    openMenu.style.display = "none";
   }
 
-
-  function minus() {
-    total -= 1;
-    document.getElementById('count-text').textContent = total;
+  function closeModal() {
+    modal.style.display = "none";
+    openMenu.style.display = "block";
   }
-
-function resetCounter() {
-  total = 0;
-  document.getElementById('count-text').textContent = total;
-}
 
