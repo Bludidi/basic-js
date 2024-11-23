67d686d1d03b0c790ee0bc0297e0153aa6eb2b7e
Bludidi <bludidi5@gmail.com>
2024-11-23 23:36:17 +0200

    Add fun corner page

--- index.js
+++ index.js
@@ -21,17 +21,17 @@
 
 /*Nav and Modal */
 
-const openMenu = document.querySelector(".open-modal");
-const closeMenu = document.querySelector(".close-modal");
-const modal = document.querySelector(".modal");
+// const openMenu = document.querySelector(".open-modal");
+// const closeMenu = document.querySelector(".close-modal");
+// const modal = document.querySelector(".modal");
 
-function openModal() {
-    modal.style.display = "block";
-    openMenu.style.display = "none";
-  }
+// function openModal() {
+//     modal.style.display = "block";
+//     openMenu.style.display = "none";
+//   }
 
-  function closeModal() {
-    modal.style.display = "none";
-    openMenu.style.display = "block";
-  }
+//   function closeModal() {
+//     modal.style.display = "none";
+//     openMenu.style.display = "block";
+//   }
 
